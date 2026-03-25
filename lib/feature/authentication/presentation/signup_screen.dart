7d973ef6150693a830eb4fdd47d033e0cd8cef08
LsLisan <mdlisan36@gmail.com>
2026-03-25 17:23:09 +0600

    Signup Done

--- lib/feature/authentication/presentation/signup_screen.dart
+++ lib/feature/authentication/presentation/signup_screen.dart
@@ -1,4 +1,5 @@
- import 'package:b2b_solution/core/utils/local_assets/icon_path.dart';
+ import 'package:b2b_solution/core/gloabal/custom_button.dart';
+import 'package:b2b_solution/core/utils/local_assets/icon_path.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -56,6 +57,7 @@ class SignupScreen extends ConsumerWidget{
                 onChanged: (value) => controller.updateLegalName(value),
                 hintText: "Ex. 101010 Ontario inc",
                 hintTextColor: AppColor.grey400,
+                textColor: Colors.black,
                 borderRadius: 12.r,
               ),
 
@@ -72,6 +74,8 @@ class SignupScreen extends ConsumerWidget{
                 onChanged: (value) => controller.updateBusinessName(value),
                 hintText: "Ex. Subway",
                 hintTextColor: AppColor.grey400,
+                textColor: Colors.black,
+
                 borderRadius: 12.r,
               ),
 
@@ -88,6 +92,8 @@ class SignupScreen extends ConsumerWidget{
                 onChanged: (value) => controller.updateName(value),
                 hintText: "Ex. John smith",
                 hintTextColor: AppColor.grey400,
+                textColor: Colors.black,
+
                 borderRadius: 12.r,
               ),
 
@@ -104,6 +110,8 @@ class SignupScreen extends ConsumerWidget{
                 onChanged: (value) => controller.updateEmail(value),
                 hintText: "Ex. Jhonsmith@gmail.com",
                 hintTextColor: AppColor.grey400,
+                textColor: Colors.black,
+
                 borderRadius: 12.r,
               ),
 
@@ -121,6 +129,8 @@ class SignupScreen extends ConsumerWidget{
                 onChanged: (value) => controller.updatePosition(value),
                 hintText: "Ex. Owner",
                 hintTextColor: AppColor.grey400,
+                textColor: Colors.black,
+
                 borderRadius: 12.r,
               ),
 
@@ -137,6 +147,8 @@ class SignupScreen extends ConsumerWidget{
                 onChanged: (value) => controller.updateFoodCategory(value),
                 hintText: "Ex. Sandwich",
                 hintTextColor: AppColor.grey400,
+                textColor: Colors.black,
+
                 borderRadius: 12.r,
               ),
 
@@ -153,36 +165,137 @@ class SignupScreen extends ConsumerWidget{
                 onChanged: (value) => controller.updateYearsOfOperation(value),
                 hintText: "Ex. Sandwich",
                 hintTextColor: AppColor.grey400,
+                textColor: Colors.black,
+
                 borderRadius: 12.r,
               ),
 
               SizedBox(height: 16.h,),
-              Row(
-                children: [
-                  CustomText(
-                    text :"Business image",
+
+              CustomText(
+                    text :"Profile image",
                     fontSize: 16.sp,
                     fontWeight: FontWeight.w500,
                     color: Colors.black,
-                  ),
-                  CustomText(
-                    text: " (Branding)",
-                    fontSize: 16.sp,
-                    fontWeight: FontWeight.w500,
-                    color: AppColor.grey400,
-                  )
-                ],
               ),
+
               SizedBox(height: 12.h,),
               CustomImagePickerCard(
-                title: "Business image",
+                title: "Profile image",
                 imagePath: state.businessImage,
                 onPickImage: () {
                   _showImageSourceSheet(context, (source) {
-                    ref.read(signupProvider.notifier).pickBusinessImage(source);
+                    ref.read(signupProvider.notifier).pickProfileImage(source);
                   });
                 },
               ),
+
+              SizedBox(height: 16.h,),
+              CustomText(
+                text: "Upload business license",
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w500,
+                color: Colors.black,
+              ),
+              SizedBox(height: 12.h,),
+              CustomImagePickerCard(
+                title: "Upload business license",
+                imagePath: state.businessImage,
+                onPickImage: () {
+                  _showImageSourceSheet(context, (source) {
+                    ref.read(signupProvider.notifier).pickLicenseImage(source);
+                  });
+                },
+              ),
+
+
+              CustomText(
+                text: "Password",
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w500,
+                color: Colors.black,
+              ),
+
+              SizedBox(height: 12.h,),
+              CustomTextFormField(
+                onChanged: (value) => controller.updatePassword(value),
+                hintText: "",
+                hintTextColor: AppColor.grey400,
+                borderRadius: 12.r,
+                textColor: Colors.black,
+
+                suffixIcon: IconButton(
+                  onPressed: () => controller.toggleVisibility(),
+                  icon: Icon(
+                    state.obscurePassword
+                        ? Icons.visibility_off
+                        : Icons.visibility,
+                    color: AppColor.grey300,
+                  ),
+                ),
+              ),
+
+              CustomText(
+                text: "Confirm Password",
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w500,
+                color: Colors.black,
+              ),
+
+              SizedBox(height: 12.h,),
+              CustomTextFormField(
+                onChanged: (value) => controller.updateConfirmPassword(value),
+                hintText: "",
+                hintTextColor: AppColor.grey400,
+                textColor: Colors.black,
+
+                borderRadius: 12.r,
+                suffixIcon: IconButton(
+                  onPressed: () => controller.toggleConfirmPasswordVisibility(),
+                  icon: Icon(
+                    state.obscurePassword
+                        ? Icons.visibility_off
+                        : Icons.visibility,
+                    color: AppColor.grey300,
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 20.h,),
+              CustomButton(
+                backgroundColor: AppColor.primary,
+                borderRadius: 16.r,
+                text: "Sign Up",
+                textColor: Colors.black,
+                onPressed: (){
+                  print("Sign Up Tapped");
+                },
+              ),
+
+
+              SizedBox(height: 12.h,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomText(
+                      text: "Already have an account?",
+                    fontSize: 14.sp,
+                    color: AppColor.grey400,
+                  ),
+                  SizedBox(width: 8.w,),
+                  GestureDetector(
+                    onTap: (){
+
+                    },
+                    child: CustomText(
+                      text: "Sign in",
+                      fontSize: 14.sp,
+                      color: AppColor.secondary
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 48.h,),
             ],
           )
         ),
--- lib/feature/authentication/widgets/custom_image_picker_card.dart
+++ lib/feature/authentication/widgets/custom_image_picker_card.dart
@@ -1,10 +1,9 @@
 import 'dart:io';
 import 'package:b2b_solution/core/design_system/app_color.dart';
-import 'package:b2b_solution/core/gloabal/custom_text.dart';
-import 'package:b2b_solution/core/utils/local_assets/icon_path.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:b2b_solution/core/utils/local_assets/icon_path.dart';
 
 class CustomImagePickerCard extends StatelessWidget {
   final String title;
@@ -20,77 +19,76 @@ class CustomImagePickerCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool hasImage = imagePath != null && imagePath!.isNotEmpty;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        GestureDetector(
+          onTap: onPickImage, // Single tap listener for the whole area
+          child: Container(
+            width: double.infinity,
+            height: 200.h,
+            decoration: BoxDecoration(
+              color: const Color(0xFFF8FAFC), // Slight off-white background
+              borderRadius: BorderRadius.circular(16.r),
+              border: Border.all(color: const Color(0xFFE2E8F0)),
+              image: hasImage
+                  ? DecorationImage(
+                image: FileImage(File(imagePath!)),
+                fit: BoxFit.cover,
+              )
+                  : null,
+            ),
+            child: hasImage
+                ? const SizedBox.shrink() // Clean view: Tap on image triggers onPickImage
+                : _buildPlaceholder(),   // Placeholder view for empty state
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPlaceholder() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(IconPath.galleryAdd, height: 44.h, width: 44.w),
+        SizedBox(height: 16.h),
+        Text(
+          "Upload $title",
+          style: GoogleFonts.poppins(
+            fontSize: 18.sp,
+            fontWeight: FontWeight.w600,
+            color: const Color(0xFF1E293B),
+          ),
+        ),
+        Text(
+          "Supports: JPG, PNG",
+          style: GoogleFonts.poppins(
+            fontSize: 14.sp,
+            color: const Color(0xFF64748B),
+          ),
+        ),
+        SizedBox(height: 16.h),
+        // The button only exists as a visual guide in the placeholder
         Container(
           width: double.infinity,
-          padding: EdgeInsets.all(24.w),
+          padding: EdgeInsets.symmetric( vertical: 10.h),
+          margin: EdgeInsets.symmetric(horizontal: 20.h),
           decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(16.r),
-            border: Border.all(color: const Color(0xFFE2E8F0)),
+            color: AppColor.primary,
+            borderRadius: BorderRadius.circular(12.r),
           ),
-          child: Column(
-            children: [
-              // Preview or Placeholder Icon
-              if (imagePath != null && imagePath!.isNotEmpty)
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(12.r),
-                  child: Image.file(
-                    File(imagePath!),
-                    height: 100.h,
-                    width: 100.w,
-                    fit: BoxFit.cover,
-                  ),
-                )
-              else
-                Image.asset(IconPath.galleryAdd,height: 44.h,width: 44.w,),
-
-              SizedBox(height: 16.h),
-
-              Text(
-                imagePath != null && imagePath!.isNotEmpty
-                    ? "Image Selected"
-                    : "Upload $title",
-                style: GoogleFonts.poppins(
-                  fontSize: 18.sp,
-                  fontWeight: FontWeight.w600,
-                  color: const Color(0xFF1E293B),
-                ),
+          child: Center(
+            child: Text(
+              "Choose Picture",
+              style: GoogleFonts.poppins(
+                fontWeight: FontWeight.w600,
+                fontSize: 14.sp,
+                color: Colors.black,
               ),
-
-              Text(
-                "Supports: JPG, PNG",
-                style: GoogleFonts.poppins(
-                  fontSize: 14.sp,
-                  color: const Color(0xFF64748B),
-                ),
-              ),
-
-              SizedBox(height: 24.h),
-
-              // Action Button
-              SizedBox(
-                width: double.infinity,
-                height: 48.h,
-                child: ElevatedButton(
-                  onPressed: onPickImage,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColor.primary,
-                    foregroundColor: Colors.black,
-                    elevation: 0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.r),
-                    ),
-                  ),
-                  child: Text(
-                    imagePath != null && imagePath!.isNotEmpty ? "Change Picture" : "Choose Picture",
-                    style: GoogleFonts.poppins(fontWeight: FontWeight.w600),
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ],
