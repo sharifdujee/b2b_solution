089ca795b25783520d1810d69a67ecc6bc060d0d
LsLisan <mdlisan36@gmail.com>
2026-03-26 09:35:14 +0600

    Reset pass Done

--- lib/feature/authentication/provider/reset_password_provider.dart
+++ lib/feature/authentication/provider/reset_password_provider.dart
@@ -4,4 +4,37 @@ import 'package:flutter_riverpod/legacy.dart';
 class ResetPasswordNotifier extends StateNotifier<ResetPasswordStateModel> {
   ResetPasswordNotifier() : super(ResetPasswordStateModel());
 
+  void updateEmail(String email) {
+    state = state.copyWith(email: email, clearError: true);
+  }
+
+  void updateVerificationCode(String verificationCode) {
+    state = state.copyWith(verificationCode: verificationCode, clearError: true);
+  }
+
+  void updatePassword(String password) {
+    state = state.copyWith(password: password, clearError: true);
+  }
+
+  void updateConfirmPassword(String confirmPassword) {
+    state = state.copyWith(confirmPassword: confirmPassword, clearError: true);
+  }
+
+  void toggleVisibility() {
+    state = state.copyWith(obscurePassword: !state.obscurePassword);
+  }
+
+  void toggleConfirmPasswordVisibility() {
+    state = state.copyWith(obscureConfirmPassword: !state.obscureConfirmPassword);
+  }
+
+  void toggleIsLoading() {
+    state = state.copyWith(isLoading: !state.isLoading);
+  }
+
+
 }
+
+final ResetPasswordProvider = StateNotifierProvider.autoDispose<ResetPasswordNotifier, ResetPasswordStateModel>((ref) {
+  return ResetPasswordNotifier();
+});
