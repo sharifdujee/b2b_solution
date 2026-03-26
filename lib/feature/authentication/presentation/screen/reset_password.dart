559ac58fa6095fed00bbe4792540a4f6cb6ed99e
“sharifdujee” <sharifdu44@gmail.com>
2026-03-26 12:58:14 +0600

    reset password conflict resolve

--- lib/feature/authentication/presentation/screen/reset_password.dart
+++ lib/feature/authentication/presentation/screen/reset_password.dart
@@ -7,14 +7,11 @@ import 'package:go_router/go_router.dart';
 import '../../../../core/design_system/app_color.dart';
 
 import '../../../../core/utils/local_assets/icon_path.dart';
-import '../../provider/reset_password_provider.dart';
+
 
 
 class ResetPassword extends ConsumerWidget {
-import '../../../../core/gloabal/custom_text_form_field.dart';
-import '../../../../core/utils/local_assets/icon_path.dart';
 
-class ResetPassword extends ConsumerWidget{
 
   const ResetPassword({super.key});
 
